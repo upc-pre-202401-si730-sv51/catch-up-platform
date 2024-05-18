04178eff933a42cba4db680d238b0e8868d2e68b
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-18 07:08:25 -0500

    chore: added database context.

---
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,29 @@
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        base.OnConfiguring(builder);
+        builder.AddCreatedUpdatedInterceptor();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.Entity<FavoriteSource>().ToTable("FavoriteSources");
+        builder.Entity<FavoriteSource>().HasKey(f => f.Id);
+        builder.Entity<FavoriteSource>().Property(f => f.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<FavoriteSource>().Property(f => f.NewsApiKey).IsRequired();
+        builder.Entity<FavoriteSource>().Property(f => f.SourceId).IsRequired();
+        
+        builder.UseSnakeCaseNamingConvention();
+
+    }
+}
